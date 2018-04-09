0a267c53e0c5c55c01e8cee2a40b69036093931a
clamli <boyizjuer2017@gmail.com>
2018-04-09 18:10:25 +0800

    20180409

---
+++ Array/Solution for Rotate Array.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+import java.util.List;
+import java.awt.font.GraphicAttribute;
+import java.lang.reflect.Array;
+import java.security.AlgorithmConstraints;
+import java.text.DateFormatSymbols;
+import java.time.format.ResolverStyle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Formatter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.logging.Logger;
+
+import javax.naming.SizeLimitExceededException;
+import javax.naming.ldap.StartTlsRequest;
+import javax.naming.spi.DirStateFactory.Result;
+import javax.print.attribute.standard.OutputDeviceAssigned;
+import javax.swing.DefaultFocusManager;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+import javax.swing.text.DefaultEditorKit.InsertBreakAction;
+import javax.tools.ToolProvider;
+import javax.xml.ws.Endpoint;
+
+import org.omg.CORBA.Current;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.w3c.dom.css.ElementCSSInlineStyle;
+
+class Solution {
+	public void rotate(int[] nums, int k) {
+		int size = nums.length;
+		reverse(nums, 0, size - 1);
+		reverse(nums, 0, k%size - 1);
+		reverse(nums, k%size, size - 1);
+    }
+	
+	private void reverse(int[] nums, int start, int end) {
+		while (start < end) {
+			int tmp = nums[start];
+			nums[start] = nums[end];
+			nums[end] = tmp;
+			start++;
+			end--;
+		}
+	}
+}
