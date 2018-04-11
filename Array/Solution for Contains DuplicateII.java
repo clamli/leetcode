da8e7d69d045318245ccf5a2ca6ceb2afe0d6d4e
clamli <boyizjuer2017@gmail.com>
2018-04-11 22:25:50 +0800

    20180411

---
+++ Array/Solution for Contains DuplicateII.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+import java.util.List;
+import java.awt.font.GraphicAttribute;
+import java.awt.font.NumericShaper;
+import java.lang.reflect.Array;
+import java.security.AlgorithmConstraints;
+import java.text.DateFormatSymbols;
+import java.time.format.ResolverStyle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Formatter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.logging.Logger;
+
+import javax.naming.SizeLimitExceededException;
+import javax.naming.ldap.StartTlsRequest;
+import javax.naming.spi.DirStateFactory.Result;
+import javax.print.attribute.standard.OutputDeviceAssigned;
+import javax.swing.DefaultFocusManager;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+import javax.swing.text.DefaultEditorKit.InsertBreakAction;
+import javax.tools.ToolProvider;
+import javax.xml.ws.Endpoint;
+
+import org.omg.CORBA.Current;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.w3c.dom.css.ElementCSSInlineStyle;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+        	if (map.containsKey(nums[i])) {
+        		if (i - map.get(nums[i]) <= k)
+        			return true;
+        	}
+        	map.put(nums[i], i);
+        }
+        return false;
+    }
+}
