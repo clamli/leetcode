c4df05c85b18d01c3ad7eb911a3dc18eda0189f9
clamli <boyizjuer2017@gmail.com>
2018-05-06 16:18:21 +0800

    20180506

---
+++ Array/Solution for Maximum Product of Three Numbers.java
@@ -0,0 +1,57 @@
+package leetcode;
+
+import java.util.List;
+import java.awt.font.GraphicAttribute;
+import java.awt.font.NumericShaper;
+import java.io.FilterInputStream;
+import java.lang.reflect.Array;
+import java.security.AlgorithmConstraints;
+import java.text.DateFormatSymbols;
+import java.time.format.ResolverStyle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Formatter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Set;
+import java.util.Stack;
+import java.util.TreeSet;
+import java.util.logging.Logger;
+
+import javax.naming.SizeLimitExceededException;
+import javax.naming.ldap.StartTlsRequest;
+import javax.naming.spi.DirStateFactory.Result;
+import javax.print.attribute.standard.OutputDeviceAssigned;
+import javax.swing.DefaultFocusManager;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+import javax.swing.text.DefaultEditorKit.InsertBreakAction;
+import javax.tools.ToolProvider;
+import javax.xml.ws.Endpoint;
+
+import org.omg.CORBA.Current;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.w3c.dom.css.ElementCSSInlineStyle;
+
+class Solution {
+	public double findMaxAverage(int[] nums, int k) {
+        int max, sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        max = sum;
+        for (int i = 1; i <= nums.length-k; i++) {
+            sum = sum - nums[i-1] + nums[i+k-1];
+            max = Math.max(max, sum);
+        }
+        return max*1.0 / k;
+    }
+}
