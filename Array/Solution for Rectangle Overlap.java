37b2defc06b7cc4169b7ffcacb5a122f80604efd
clamli <boyizjuer2017@gmail.com>
2018-05-20 11:34:23 +0800

    20180520

---
+++ Array/Solution for Rectangle Overlap.java
@@ -0,0 +1,54 @@
+package leetcode;
+
+import java.util.List;
+import java.awt.font.GraphicAttribute;
+import java.awt.font.NumericShaper;
+import java.io.FilterInputStream;
+import java.lang.reflect.Array;
+import java.security.AlgorithmConstraints;
+import java.text.DateFormatSymbols;
+import java.time.format.ResolverStyle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Formatter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Set;
+import java.util.Stack;
+import java.util.TreeSet;
+import java.util.logging.Logger;
+
+import javax.naming.SizeLimitExceededException;
+import javax.naming.ldap.StartTlsRequest;
+import javax.naming.spi.DirStateFactory.Result;
+import javax.print.attribute.standard.OutputDeviceAssigned;
+import javax.swing.DefaultFocusManager;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+import javax.swing.text.DefaultEditorKit.InsertBreakAction;
+import javax.tools.ToolProvider;
+import javax.xml.ws.Endpoint;
+
+import org.omg.CORBA.Current;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.w3c.dom.css.ElementCSSInlineStyle;
+
+class Solution {
+	public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+		int maxx1 = Math.max(rec1[0], rec2[0]);
+		int minx2 = Math.min(rec1[2], rec2[2]);
+		int maxy1 = Math.max(rec1[1], rec2[1]);
+		int miny2 = Math.min(rec1[3], rec2[3]);
+		if (minx2 > maxx1 && miny2 > maxy1)
+			return true;
+		return false;
+	}
+}
