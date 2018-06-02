dd7f9287483e746bf0c4f902aa1a1bd7cdd1e4a0
clamli <boyizjuer2017@gmail.com>
2018-06-02 17:42:54 +0800

    20180602

---
+++ Array/Solution for Largest Number At Least Twice of Others.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import java.util.List;
+import java.awt.font.GraphicAttribute;
+import java.lang.reflect.Array;
+import java.security.AlgorithmConstraints;
+import java.text.DateFormatSymbols;
+import java.time.format.ResolverStyle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Stack;
+
+import javax.naming.SizeLimitExceededException;
+import javax.naming.ldap.StartTlsRequest;
+import javax.naming.spi.DirStateFactory.Result;
+import javax.print.attribute.standard.OutputDeviceAssigned;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+import javax.tools.ToolProvider;
+import javax.xml.ws.Endpoint;
+
+import org.omg.CORBA.Current;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.w3c.dom.css.ElementCSSInlineStyle;
+
+class Solution {
+    
+    public int dominantIndex(int[] nums) {
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MAX_VALUE, maxind = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max1) {
+                max2 = max1;
+                max1 = nums[i];
+                maxind = i;
+            } else if (nums[i] > max2) {
+                max2 = nums[i];
+            }
+        }
+        if (max1 >= 2*max2)
+            return maxind;
+        return -1;
+    }
+    
+}
