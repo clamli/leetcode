4609f2d9893051d56615e3216451ea31951f1ee2
clamli <boyizjuer2017@gmail.com>
2018-06-04 16:50:54 +0800

    20180604

---
+++ Array/Solution for Max Chunks To Make Sorted.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+import java.util.List;
+import java.awt.font.GraphicAttribute;
+import java.lang.reflect.Array;
+import java.security.AlgorithmConstraints;
+import java.text.DateFormatSymbols;
+import java.time.format.ResolverStyle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Stack;
+
+import javax.naming.SizeLimitExceededException;
+import javax.naming.ldap.StartTlsRequest;
+import javax.naming.spi.DirStateFactory.Result;
+import javax.print.attribute.standard.OutputDeviceAssigned;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+import javax.tools.ToolProvider;
+import javax.xml.ws.Endpoint;
+
+import org.omg.CORBA.Current;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.w3c.dom.css.ElementCSSInlineStyle;
+
+class Solution {
+    
+    public int maxChunksToSorted(int[] arr) {
+        int max = 0, cnt = 0;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+            if (max == i) {
+                cnt++; 
+            }
+        }
+        return cnt;
+    }
+    
+}
