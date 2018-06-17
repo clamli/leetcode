211aeaf4f805b77a75c316277aa80c1fd48d50c9
clamli <boyizjuer2017@gmail.com>
2018-06-17 17:15:22 +0800

    20180617

---
+++ Array/Solution for Peak Index in a Mountain Array.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import java.util.List;
+import java.awt.font.GraphicAttribute;
+import java.lang.reflect.Array;
+import java.security.AlgorithmConstraints;
+import java.text.DateFormatSymbols;
+import java.time.format.ResolverStyle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Stack;
+
+import javax.naming.SizeLimitExceededException;
+import javax.naming.ldap.StartTlsRequest;
+import javax.naming.spi.DirStateFactory.Result;
+import javax.print.attribute.standard.OutputDeviceAssigned;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+import javax.tools.ToolProvider;
+import javax.xml.ws.Endpoint;
+
+import org.omg.CORBA.Current;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.w3c.dom.css.ElementCSSInlineStyle;
+
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int ind = 0;
+        for (int i = 1; i < A.length-1; i++) {
+            if (A[i+1] < A[i]) {
+                ind = i;
+                break;
+            }
+        }
+        return ind;
+    }
+}
