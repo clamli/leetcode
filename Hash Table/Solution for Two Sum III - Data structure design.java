8f5e6835217feedc4517281fa4477e54f3825b56
clamli <boyizjuer2017@gmail.com>
2018-06-27 11:38:03 +0800

    20180627

---
+++ Hash Table/Solution for Two Sum III - Data structure design.java
@@ -0,0 +1,59 @@
+package leetcode;
+
+import java.util.List;
+import java.awt.font.GraphicAttribute;
+import java.lang.reflect.Array;
+import java.security.AlgorithmConstraints;
+import java.text.DateFormatSymbols;
+import java.time.format.ResolverStyle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Stack;
+
+import javax.naming.SizeLimitExceededException;
+import javax.naming.ldap.StartTlsRequest;
+import javax.naming.spi.DirStateFactory.Result;
+import javax.print.attribute.standard.OutputDeviceAssigned;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+import javax.tools.ToolProvider;
+import javax.xml.ws.Endpoint;
+
+import org.omg.CORBA.Current;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.w3c.dom.css.ElementCSSInlineStyle;
+
+class Solution {
+    Map<Integer, Integer> map;
+    
+    /** Initialize your data structure here. */
+    public TwoSum() {
+        map = new HashMap<>();
+    }
+    
+    /** Add the number to an internal data structure.. */
+    public void add(int number) {
+        map.put(number, map.getOrDefault(number, 0)+1);
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    public boolean find(int value) {
+        for (int num : map.keySet()) {
+            int key = value - num;
+            if (map.containsKey(key) && !(key == num && map.get(key) == 1)) return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * boolean param_2 = obj.find(value);
+ */
