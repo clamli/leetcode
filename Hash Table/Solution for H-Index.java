cc44e01b6f08a8b6f0e180e13b799e285ce654ea
clamli <boyizjuer2017@gmail.com>
2018-07-03 16:08:01 +0800

    20180703

---
+++ Hash Table/Solution for H-Index.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+import java.util.List;
+import java.awt.font.GraphicAttribute;
+import java.lang.reflect.Array;
+import java.security.AlgorithmConstraints;
+import java.text.DateFormatSymbols;
+import java.time.format.ResolverStyle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Stack;
+
+import javax.naming.SizeLimitExceededException;
+import javax.naming.ldap.StartTlsRequest;
+import javax.naming.spi.DirStateFactory.Result;
+import javax.print.attribute.standard.OutputDeviceAssigned;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+import javax.tools.ToolProvider;
+import javax.xml.ws.Endpoint;
+
+import org.omg.CORBA.Current;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.w3c.dom.css.ElementCSSInlineStyle;
+
+class Solution {
+    public int hIndex(int[] citations) {
+        int N = citations.length;
+        int[] count = new int[N+1];
+        for (int cite : citations) {
+            if (cite >= N) count[N]++;
+            else count[cite]++;
+        }
+        int sum = 0, i;
+        for (i = N; i >= 0; i--) {
+            sum += count[i];
+            if (sum >= i) break;
+        }
+        return i;
+    }
+}
