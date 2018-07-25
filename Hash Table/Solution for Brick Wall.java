fe8be6b90b53f20d8cee980e5ec867c7d663e1f8
clamli <boyizjuer2017@gmail.com>
2018-07-25 15:18:09 +0800

    20180725

---
+++ Hash Table/Solution for Brick Wall.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+import java.util.List;
+import java.awt.font.GraphicAttribute;
+import java.lang.reflect.Array;
+import java.security.AlgorithmConstraints;
+import java.text.DateFormatSymbols;
+import java.time.format.ResolverStyle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Stack;
+
+import javax.naming.SizeLimitExceededException;
+import javax.naming.ldap.StartTlsRequest;
+import javax.naming.spi.DirStateFactory.Result;
+import javax.print.attribute.standard.OutputDeviceAssigned;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+import javax.tools.ToolProvider;
+import javax.xml.ws.Endpoint;
+
+import org.omg.CORBA.Current;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.w3c.dom.css.ElementCSSInlineStyle;
+
+class Solution {
+    public int leastBricks(List<List<Integer>> wall) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+        for (List<Integer> row : wall) {
+            int start = 0;
+            for (int i = 0; i < row.size()-1; i++) {
+                int idx = start+row.get(i);
+                map.put(idx, map.getOrDefault(idx, 0)+1);
+                max = Math.max(max, map.get(idx));
+                start = idx;
+            }
+        }
+        return wall.size()-max;
+    }
+}
