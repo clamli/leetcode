874937b698d1975a0e30eed4f61fbd7af0cc5729
clamli <boyizjuer2017@gmail.com>
2018-08-09 17:02:24 +0800

    20180809

---
+++ Divide and Conquer/Solution for Search a 2D Matrix II.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+import java.util.List;
+import java.awt.font.GraphicAttribute;
+import java.lang.reflect.Array;
+import java.security.AlgorithmConstraints;
+import java.text.DateFormatSymbols;
+import java.time.format.ResolverStyle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Stack;
+
+import javax.naming.SizeLimitExceededException;
+import javax.naming.ldap.StartTlsRequest;
+import javax.naming.spi.DirStateFactory.Result;
+import javax.print.attribute.standard.OutputDeviceAssigned;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+import javax.tools.ToolProvider;
+import javax.xml.ws.Endpoint;
+
+import org.omg.CORBA.Current;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.w3c.dom.css.ElementCSSInlineStyle;
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) return false;
+        int r = 0;
+        int c = matrix[0].length-1;
+        while (r < matrix.length && c >= 0) {
+            if (target == matrix[r][c]) return true;
+            else if (target < matrix[r][c]) c--;
+            else r++;
+        }
+        return false;
+    }
+}
